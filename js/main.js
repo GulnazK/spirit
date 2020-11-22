b251bddb26a010e98223bd6011a4ca6d1aad759b
Gulnaz Kaparova <gulnazkaparova@gmail.com>
2020-11-22 04:31:22 +0400

    spirit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.querySelector('.mobile-menu').addEventListener('click',() => {
+    document.querySelector('.top-menu').classList.toggle('show')
+})
+
+
+
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:2,
+        margin:30,
+        nav:true,
+        slideTransition: String,
+
+    });
+
+
+
+});
